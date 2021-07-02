0f1a95308452a5350327e7a5a62dafeaa45bdd2b
Sergey Silenok <silenoksergey34@gmail.com>
2021-07-02 20:00:14 +0300

    added a script

---
+++ home_work_1_20/script.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd foldername
+mkdir foldername_1 foldername_2 foldername_3
+cd foldername_1
+touch text_1.txt text_2.txt text_3.txt name_1.json name_2.json
+mkdir folder_1 folder_2 folder_3
+ls
+mv text_1.txt folder_1
+mv name_1.json folder_1
